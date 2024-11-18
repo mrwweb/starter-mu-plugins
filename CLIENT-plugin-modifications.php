73b9af04f1d778c38e81928868b1129d799cd96d
Mark Root-Wiley <info@mrwweb.com>
2024-11-18 13:26:22 -0800

    errant words removed

--- CLIENT-plugin-modifications.php
+++ CLIENT-plugin-modifications.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Modifications to plugin behavior and output
-Description: Central location for changing behavior of plugins on trec.org. Plugins modified: Advanced Custom Fields PRO, Archived Post Status, WP-CFM
+Description: Central location for changing behavior of plugins. Plugins modified: Advanced Custom Fields PRO, Archived Post Status, WP-CFM
 Version: 1.0.0
 Author: Mark Root-Wiley, MRW Web Design
 Author URI: https://MRWweb.com
