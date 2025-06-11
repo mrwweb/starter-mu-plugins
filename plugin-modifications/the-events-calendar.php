b7fe116ea82fc0b69c2caaf9c129d9d75154d496
Mark Root-Wiley <info@mrwweb.com>
2025-06-11 07:30:49 -0700

    update tec-reset functions

--- plugin-modifications/the-events-calendar.php
+++ plugin-modifications/the-events-calendar.php
@@ -1,196 +1,170 @@
 <?php
-/**
- * Plugin Name: The Events Calendar Customizations
- * Description: An assortment of changes to how The Events Calendar behaves. You are encouraged to delete, add, and revise this file to meet your needs!
- * Version: 2.1.0
- * Author: Mark Root-Wiley, MRW Web Design
- * Author URI: https://MRWweb.com
- * Plugin URI: https://github.com/mrwweb/the-events-calendar-reset
- *
- * @package _mrw-mu-plugins
- */
+/*
+Plugin Name: The Events Calendar Customizations
+Description: An assortment of changes to how The Events Calendar behaves. You are encouraged to delete, add, and revise this file to meet your needs!
+Version: 2.2.1
+Author: Mark Root-Wiley, MRW Web Design
+Author URI: https://MRWweb.com
+Plugin URI: https://github.com/mrwweb/the-events-calendar-reset
+*/
 
-namespace _MRW_\TEC;
+namespace MRW\TEC;
 
 /**
- * Useful helper function to detect Tribe Events pages in your theme
+ * Useful helper function to detect Events pages in your theme
  *
  * Modified slightly from the link
  *
- * Usage: \TEC\is_tribe_view()
+ * Usage: \MRW\TEC\is_tec_view()
  *
  * @link https://gist.github.com/samkent/b98bd3c9b28426b8461bc1417adf7b5d
  */
-function is_tribe_view() {
-	return (
-			function_exists( 'tribe_is_event' ) &&
-			tribe_is_event()
-		) ||
+function is_tec_view() {
+	return
+		\function_exists( 'tec_is_view' ) &&
 		(
-			function_exists( 'tribe_is_event_category' ) &&
-			tribe_is_event_category()
-		) ||
-		(
-			function_exists( 'tribe_is_in_main_loop' ) &&
-			tribe_is_in_main_loop()
-		) ||
-		(
-			function_exists( 'tec_is_view' ) &&
-			tec_is_view()
-		) ||
-		'tribe_events' === get_post_type() ||
-		is_singular( 'tribe_events' );
+			\tec_is_view() ||
+			'tribe_events' === get_post_type() ||
+			\is_singular( 'tribe_events' ) ||
+			\tribe_is_event() ||
+			\tribe_is_event_category() ||
+			\tribe_is_in_main_loop() ||
+			(
+				function_exists( 'tribe_is_event_series' ) && \tribe_is_event_series( get_the_ID() )
+			)
+		);
 }
 
-add_filter( 'tribe_events_views_v2_bootstrap_html', __NAMESPACE__ . '\replace_section_with_div' );
 /**
  * Replace the wrapping <section> element surrounding all output with a <div>
- *
- * @param stringl $html the HTML output of the view
- *
- * @return string the HTML output of the view with <section> replaced with <div>
  */
+\add_filter( 'tribe_events_views_v2_bootstrap_html', __NAMESPACE__ . '\replace_section_with_div' );
 function replace_section_with_div( $html ) {
-	return str_replace( '<section', '<div', str_replace( '</section>', '</div>', $html ) );
+	return \str_replace( '<section', '<div', \str_replace( '</section>', '</div>', $html ) );
 }
 
-add_filter( 'tribe_get_organizer_website_link_label', __NAMESPACE__ . '\return_organizer_name', 99 );
+\add_filter( 'tribe_get_organizer_website_link_label', __NAMESPACE__ . '\return_organizer_name', 99 );
 /**
  * Use Organizer name for Event Organizer Website Link
- *
- * @return string organizer title/name
  */
 function return_organizer_name() {
-	return tribe_get_organizer();
+	return \tribe_get_organizer();
 }
 
-add_filter( 'tribe_get_venue_link', __NAMESPACE__ . '\venue_link', 10, 4 );
-/**
- * Link Venue Name to Website instead of venue
- *
- * @param string $link the HTML link to the venue
- * @param int    $venue_id the ID of the venue
- * @param bool   $full_link whether to return HTML markup (true) or URL (false)
- * @param string $url the URL of the venue
- *
- * @return string the modified HTML link to the venue
- */
-function venue_link( $link, $venue_id, $full_link, $url ) {
-	if ( $full_link ) {
-		$venue_name = tribe_get_venue( $venue_id );
-		$website    = tribe_get_venue_website_url( $venue_id );
-		if ( $website ) {
-			$link = str_replace( $url, $website, $link );
-			$link = str_replace( 'title="' . $venue_name . '"', '', $link );
-		}
-	}
-	return $link;
-}
-
-add_filter( 'register_tribe_events_post_type_args', __NAMESPACE__ . '\no_tags_on_events' );
+\add_filter( 'register_tribe_events_post_type_args', __NAMESPACE__ . '\no_tags_on_events' );
 /**
  * Remove post_tag from tribe_events post_type
- *
- * @param array $args the arguments for the tribe_events post type
  */
 function no_tags_on_events( $args ) {
-	$args['taxonomies'] = [];
+    $args['taxonomies'] = [];
 
-	return $args;
+    return $args;
 }
 
-add_action( 'pre_get_posts', __NAMESPACE__ . '\no_events_in_tag_archives' );
+\add_action( 'pre_get_posts', __NAMESPACE__ . '\no_events_in_tag_archives' );
 /**
  * Exclude tribe_events posts from tag archives
- *
- * @param WP_Query $query query
  */
 function no_events_in_tag_archives( $query ) {
-	if ( ! is_admin() && ! $query->is_main_query() ) {
+	if( ! \is_admin() && ! $query->is_main_query() ) {
 		return;
 	}
 
-	if ( is_tag() ) {
+	if( \is_tag() ) {
 		$query->set( 'post_type', [ 'post' ] );
 	}
 }
 
+\add_filter( 'tribe_events_editor_default_template', __NAMESPACE__ . '\default_blocks', 11 );
+/**
+ * Change default blocks when creating a new event with the Block Editor
+ *
+ * @see https://support.theeventscalendar.com/807454-Change-the-Default-Event-Template-in-Block-Editor
+ */
+function default_blocks( $template ) {
+	$template = [
+		[ 'tribe/event-datetime' ],
+		[ 'core/paragraph', [ 
+			'placeholder' => __( 'Add Description…', 'the-events-calendar' ), 
+		], ],
+		[ 'tribe/event-website' ],
+		[ 'tribe/event-venue' ],
+	];
+	/* 
+	Alternatively, you can use a theme pattern!
+	$template = array( array(
+        'core/pattern',
+        array(
+            'slug' => 'eec/default-tribe_events',
+        ),
+    ) );
+	*/
+	return $template;
+}
+
 // hide "Recent Past Events" when there are no upcoming events
-// add_filter( 'tribe_events_views_v2_show_latest_past_events_view', '__return_false' );
+\add_filter( 'tribe_events_views_v2_show_latest_past_events_view', '__return_false' );
 
 // List View
-add_filter( 'tribe_events_views_v2_view_list_template_vars', __NAMESPACE__ . '\tribe_past_reverse_chronological_v2', 100 );
+\add_filter( 'tribe_events_views_v2_view_list_template_vars', __NAMESPACE__ . '\tribe_past_reverse_chronological_v2', 100 );
+// Photo View
+\add_filter( 'tribe_events_views_v2_view_photo_template_vars', __NAMESPACE__ . '\tribe_past_reverse_chronological_v2', 100 );
 /**
  * Changes Past Event to Reverse Chronological Order
  *
  * @param array $template_vars An array of variables used to display the current view.
  *
- * @return array Same as above.
+ * @return array Same as above. 
  */
 function tribe_past_reverse_chronological_v2( $template_vars ) {
-	if ( ! empty( $template_vars['is_past'] ) ) {
-		$template_vars['events'] = array_reverse( $template_vars['events'] );
+	if ( ! \empty( $template_vars['is_past'] ) ) {
+		$template_vars['events'] = \array_reverse( $template_vars['events'] );
 	}
 
 	return $template_vars;
 }
 
-add_filter( 'tribe_template_html', __NAMESPACE__ . '\prevent_ajax_page_loads' );
+\add_filter( 'tribe_template_html', __NAMESPACE__ . '\prevent_ajax_page_loads', 10, 4 );
 /**
  * Prevent AJAX page loads by removing the data-js attribute that triggers them
- *
+ * 
  * Due to the number of changes required via PHP templates and filters (Category filters, archive titles, etc.), the AJAX page loads cause more problems than they solve
- *
- * @param string $html the HTML output of the view
- *
- * @return string the modified HTML output of the view
  */
-function prevent_ajax_page_loads( $html ) {
-	return str_replace( 'data-js="tribe-events-view-link"', '', $html );
+function prevent_ajax_page_loads( $html, $file, $name, $template ) {
+	return \str_replace( 'data-js="tribe-events-view-link"', '', $html );
 }
 
-add_filter( 'tribe_get_events_title', __NAMESPACE__ . '\archive_title', 11 );
-add_filter( 'get_the_archive_title', __NAMESPACE__ . '\archive_title', 11 );
+\add_filter( 'get_the_archive_title', __NAMESPACE__ . '\archive_title' );
 /**
  * Change "Events" to "Upcoming Events"/"Past Events" for List/Month/Day page
  *
- * Known issue: does not work with AJAX paging. Reverts to "Events"
- *
- * @param string $title the title of the archive
- *
- * @return string the modified title of the archive
+ * Known issue: does not work with AJAX page. Reverts to "Events"
  */
 function archive_title( $title ) {
-	if ( 'tribe_events' === get_post_type() ) {
+    if( 'tribe_events' === \get_post_type() ) {
 
-		if ( \tribe_is_past() || ( $_GET['eventDisplay'] ?? '' ) === 'past' ) { // phpcs:ignore I'm not actually using eventDisplay so we should be safe here
-			$title = 'Past Events';
-		} elseif ( \tribe_is_upcoming() ) {
+        if( \tribe_is_past() || ( $_GET['eventDisplay'] ?? '' ) === 'past' ) {
+            $title = 'Past Events';
+        } elseif ( \tribe_is_upcoming() ) {
 			$title = 'Upcoming Events';
 		} else {
-			$title = 'Events';
-		}
-	}
+			$title = "Events";
+        }
 
-	return $title;
+    }
+
+    return $title;
 }
 
-add_action( 'tribe_template_after_include:events/v2/components/before', __NAMESPACE__ . '\events_archive_header', 9 );
+\add_action( 'tribe_template_after_include:events/v2/components/before', __NAMESPACE__ . '\events_archive_header', 9 );
 /**
  * Add Title to Event Archive Pages
  *
  * Use Post Type Archive Descriptions plugin to edit archive description
  * https://wordpress.org/plugins/post-type-archive-descriptions/
- *
- * @return void
  */
 function events_archive_header() {
-	if ( get_post_type() !== 'tribe_event_series' ) {
-		echo '<header class="page-header is-layout-constrained alignfull">';
-		the_archive_title( '<h1 class="archive-title page-title">', '</h1>' );
-		the_archive_description( '<div class="archive-description">', '</div>' );
-		echo '</header>';
-	}
+    \the_archive_title( '<h1 class="archive-title page-title">', '</h1>' );
 }
 
 /**
@@ -198,4 +172,4 @@ function events_archive_header() {
  *
  * @see https://theeventscalendar.com/knowledgebase/k/remove-events-from-the-wordpress-admin-bar/
  */
-define( 'TRIBE_DISABLE_TOOLBAR_ITEMS', true ); //phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
+\define( 'TRIBE_DISABLE_TOOLBAR_ITEMS', true );
