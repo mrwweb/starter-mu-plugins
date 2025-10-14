5538e032bf567019bb7aa902aefb4fcb02885d9d
Mark Root-Wiley <info@mrwweb.com>
2025-10-14 07:50:22 -0700

    hide admin bar in updraftplus

--- CLIENT-plugin-modifications.php
+++ CLIENT-plugin-modifications.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Modifications to plugin behavior and output
- * Description: Central location for changing behavior of plugins. Plugins modified: Advanced Custom Fields PRO, Archived Post Status, WP-CFM
+ * Description: Central location for changing behavior of plugins. Plugins modified: Advanced Custom Fields PRO, Archived Post Status, Gravity Forms, The Events Calendar, UpdraftPlus, WP-CFM
  * Version: 1.0.0
  * Author: Mark Root-Wiley, MRW Web Design
  * Author URI: https://MRWweb.com
@@ -15,4 +15,5 @@ require_once 'plugin-modifications/advanced-custom-fields-pro.php';
 require_once 'plugin-modifications/archived-post-status.php';
 require_once 'plugin-modifications/gravity-forms.php';
 require_once 'plugin-modifications/the-events-calendar.php';
+require_once 'plugin-modifications/updraftplus.php';
 require_once 'plugin-modifications/wp-cfm.php';
---
+++ plugin-modifications/updraftplus.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Modifications to UpdraftPlus plugin
+ *
+ * @package _mrw-mu-plugins
+ */
+
+define('UPDRAFTPLUS_ADMINBAR_DISABLE', true);
