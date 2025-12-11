1eb6de21233e8ec9db495ad82c3f52313f6a4f4b
Mark Root-Wiley <info@mrwweb.com>
2025-12-11 15:34:54 -0800

    hide the Yoast SEO upsell pages

--- CLIENT-plugin-modifications.php
+++ CLIENT-plugin-modifications.php
@@ -17,3 +17,4 @@ require_once 'plugin-modifications/gravity-forms.php';
 require_once 'plugin-modifications/the-events-calendar.php';
 require_once 'plugin-modifications/updraftplus.php';
 require_once 'plugin-modifications/wp-cfm.php';
+require_once 'plugin-modifications/yoast-seo.php';
